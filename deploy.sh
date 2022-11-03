cc2248e6982b7c02bcaaf1344ae0a88809ba60de
Juraj Majerik <juro.majerik@gmail.com>
2022-11-03 17:02:25 +0100

    tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# sshcmd="ssh juraj@app.jurajmajerik.com"
-# $sshcmd "screen -S deployment"
-# $sshcmd screen -ls | grep deployment
-# $sshcmd "bash -c 'cd /home/juraj/server;git pull'"
-
-$sshcmd screen -S deployment /home/juraj/server/deploy_prod.sh
+sshcmd="ssh -t juraj@app.jurajmajerik.com"
+$sshcmd screen -S "deployment" bash /home/juraj/server/deploy_prod.sh
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+cd $HOME/server
+echo "----stopping server"
+sudo pkill server
+
+echo "----pulling"
 git pull
+
+echo "----building"
+go build
+
+echo "----starting"
+sudo ./server &
+
+echo
+echo "Command finished, exit status: $status"
+echo "Press Enter to exit."
+read

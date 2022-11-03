7d048d9b0a959a69959ab878ab2a60e3541a0b05
Juraj Majerik <juro.majerik@gmail.com>
2022-11-03 09:24:23 +0100

    tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sshcmd="ssh juraj@app.jurajmajerik.com"
-$sshcmd "bash -c 'cd /home/juraj/server;git pull'"
+# sshcmd="ssh juraj@app.jurajmajerik.com"
+# $sshcmd "screen -S deployment"
+# $sshcmd screen -ls | grep deployment
+# $sshcmd "bash -c 'cd /home/juraj/server;git pull'"
+
+$sshcmd screen -S deployment /home/juraj/server/deploy_prod.sh

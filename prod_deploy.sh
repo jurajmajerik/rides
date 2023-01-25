5b2f381a1f5ff8d7ddca8b1a5aeeae5ed83449f5
Juraj Majerik <juro.majerik@gmail.com>
2023-01-25 17:56:23 +0100

    use docker-compose in production

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -17,23 +17,8 @@ msg "Stopping Docker container"
 sudo docker stop app
 sudo docker rm app
 
-msg "Starting app container"
-sudo docker run \
--d \
---name app \
---expose 443 \
--p 443:443 \
--v /etc/letsencrypt:/etc/letsencrypt \
--e SERVER_ENV=PROD \
-app
-
-msg "Starting Postgres container"
-sudo docker run -d \
---name db-postgres \
--p 5432:5432 \
---mount type=volume,src=app-db,target=/var/lib/postgresql/data \
--e POSTGRES_PASSWORD=mysecretpassword \
-postgres:15.1-alpine
+msg "Starting containers"
+SERVER_PORT=8080 sudo docker compose up -d
 
 msg "Pruning stale Docker images"
 sudo docker image prune -f

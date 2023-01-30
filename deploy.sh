ffd34a2b0fd629f13d5a4040d4d40cfce821e031
Juraj Majerik <juro.majerik@gmail.com>
2023-01-30 16:08:30 +0100

    build

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 cd frontend
 npm run build
 cd ..
+git add .
+git commit -m "build"
+git push
 sshcmd="ssh -t juraj@app.jurajmajerik.com"
 $sshcmd screen -S "deployment" /home/juraj/app/prod_deploy.sh

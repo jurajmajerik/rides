08bbab97408dcab7f7d5163e164d8a7f676909b0
Juraj Majerik <juro.majerik@gmail.com>
2023-01-31 17:15:16 +0100

    build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import obstacles from './obstacles';
 
 const gridSize = 500;
 const gridCount = 50; // No. of squares in each direction
+const squareSize = gridSize / gridCount;
 
 // Populate points
 const points = [];
@@ -47,43 +49,157 @@ const build = async ({ x, y }) => {
 };
 build({ x: 0, y: 0 });
 
-const SVG = () => {
-  const squareSize = gridSize / gridCount;
-  const Rect = ({ type, x, y, color }) => (
+const Car = ({ x, y }) => {
+  return (
     <rect
+      className='car'
+      fill="black"
+      stroke='black'
+      key={`${x}:${y}`}
       width={squareSize}
       height={squareSize}
-      x={x}
-      y={y}
-      fill={ type === 'road' ? 'white' : (color || '#d77a61') }
-      stroke={ type === 'road' ? 'white' : (color || '#d77a61') }
+      x={x * squareSize}
+      y={y * squareSize}
+      style={{
+      }}
     />
   );
+};
 
-  const rects = [];
-  for (let [key, value] of Object.entries(points)) {
-    const [x, y] = key.split(':');
-    const { color, type } = value;
-    rects.push(
-      <Rect
-        key={`${x}:${y}`}
-        type={type}
-        x={x * squareSize}
-        y={y * squareSize}
-        color={color}
-      />
-    );
+class SVG extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: [
+        { id: 'car1', x: 0, y: 20 },
+      ]
+    };
   }
 
-  return (
-    <svg
+  async animate() {
+    const wait = (t) => new Promise((res) => {
+      setTimeout(() => {
+        res();
+      }, t);
+    });
+
+    const path = [
+      // [0,20],
+      [1,20],
+      [2,20],
+      [3,20],
+      [4,20],
+      [4,19],
+      [4,18],
+      [4,17],
+      [4,16],
+      [4,15],
+      [4,14],
+      [5,14],
+      [6,14],
+      [7,14],
+      [8,14],
+      [8,15],
+      [8,16],
+      [8,17],
+      [8,18],
+    ];
+
+    let currX = 0;
+    let currY = 20;
+
+    for (let i = 0; i < path.length; i++) {
+      const [nextX, nextY] = path[i];
+
+      // We need to look at the difference between currentX and nextX
+      
+      // Update x
+      if (currX !== nextX) {
+        // Move left or right?
+
+        // Move left - increment x
+        if (currX < nextX) {
+          while (currX < nextX) {
+            currX += 0.1;
+            await wait(1);
+            this.setState((state) => {
+              const cars = state.cars.map(({ id, x, y }) => ({ id, x: currX, y }));
+              return { cars };
+            });
+            currX += 0.10;
+          }
+        } else {
+          // Move right - decrement x
+
+        }
+      }
+
+      // Update y
+        // Move up - increment y
+        if (currY > nextY) {
+          while (currY > nextY) {
+          // for (let i = currY; i < nextX; i += 0.10) {
+            currY -= 0.1;
+            await wait(1);
+            this.setState((state) => {
+              const cars = state.cars.map(({ id, x, y }) => ({ id, x, y: currY }));
+              return { cars };
+            });
+            currY -= 0.10;
+          }
+        } else {
+          // Move down - decrement y
+
+        }
+    }
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  render() {
+    const rects = [];
+    for (let [key, value] of Object.entries(points)) {
+      const [x, y] = key.split(':');
+      const { color, type } = value;
+      rects.push(
+        <Point
+          key={`${x}:${y}`}
+          type={type}
+          x={x * squareSize}
+          y={y * squareSize}
+          color={color}
+        />
+      );
+    }
+
+    return (
+      <svg
       width={gridSize}
       height={gridSize}
-    >
-      {rects}
-    </svg>
-  )
-};
+      >
+        {rects}
+        {
+          this.state.cars.map(({ id, x, y }) => (
+            <Car key={id} x={x} y={y} />
+          ))
+        }
+      </svg>
+    );
+  }
+}
+
+const Point = ({ type, x, y, color }) => (
+  <rect
+  width={squareSize}
+  height={squareSize}
+  x={x}
+  y={y}
+  fill={ type === 'road' ? 'white' : (color || '#c1c3c7') }
+  stroke={ type === 'road' ? 'white' : (color || '#c1c3c7') }
+  />
+);
 
 function App() {
   return (

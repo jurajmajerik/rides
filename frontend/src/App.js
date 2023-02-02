45649abb1917d381c0c1edc638070cb6daa7b9a7
Juraj Majerik <juro.majerik@gmail.com>
2023-02-02 16:12:59 +0100

    clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,71 +3,38 @@ import obstacles from './obstacles';
 const gridSize = 500;
 const gridCount = 50; // No. of squares in each direction
 
-// Populate points
-const points = [];
-for (let x = 0; x < gridCount; x += 1) {
-  for (let y = 0; y < gridCount; y += 1) {
-    points[`${x}:${y}`] = { type: 'road' };
-  } 
-}
+const coordsToObstacles = [];
 obstacles.forEach(([xStart, xEnd, yStart, yEnd, color]) => {
   let x = xStart;
   while (x <= xEnd) {
     let y = yStart;
     while (y <= yEnd) {
-      points[`${x}:${y}`] = { type: 'obstacle', color };
+      coordsToObstacles[`${x}:${y}`] = color || '#d77a61';
       y += 1;
     }
     x += 1;
   }
 });
 
-// Build graph
-const graph = { '0:0': {} };
-const visited = {};
-const build = async ({ x, y }) => {
-  visited[`${x}:${y}`] = true;
-  const currentNode = graph[`${x}:${y}`];
-
-  const neighbours = [
-    [x, y - 1],
-    [x + 1, y],
-    [x, y + 1],
-    [x - 1, y],
-  ];
-
-  for (const [x, y] of neighbours) {
-    const coords = `${x}:${y}`;
-    if (points[coords]) {
-      graph[coords] = graph[coords] || {};
-      currentNode[coords] = graph[coords];
-      if (!visited[coords]) await build({ x, y });
-    }
-  }
-};
-build({ x: 0, y: 0 });
-
 const SVG = () => {
   const squareSize = gridSize / gridCount;
-  const Rect = ({ type, x, y, color }) => (
+  const Obstacle = ({ type, x, y, color }) => (
     <rect
       width={squareSize}
       height={squareSize}
       x={x}
       y={y}
-      fill={ type === 'road' ? 'white' : (color || '#d77a61') }
-      stroke={ type === 'road' ? 'white' : (color || '#d77a61') }
+      fill={color}
+      stroke={color}
     />
   );
 
-  const rects = [];
-  for (let [key, value] of Object.entries(points)) {
+  const obstacleElems = [];
+  for (let [key, color] of Object.entries(coordsToObstacles)) {
     const [x, y] = key.split(':');
-    const { color, type } = value;
-    rects.push(
-      <Rect
+    obstacleElems.push(
+      <Obstacle
         key={`${x}:${y}`}
-        type={type}
         x={x * squareSize}
         y={y * squareSize}
         color={color}
@@ -80,7 +47,7 @@ const SVG = () => {
       width={gridSize}
       height={gridSize}
     >
-      {rects}
+      {obstacleElems}
     </svg>
   )
 };

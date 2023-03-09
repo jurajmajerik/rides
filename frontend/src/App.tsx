d7390b7b49f50fa171f742f41789b7ce4d14ee20
Juraj Majerik <juro.majerik@gmail.com>
2023-03-09 14:05:15 +0100

    adjust text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,8 @@ const App = () => {
       <div className="description">
         <p className="mt-6 space-y-7 text-sm text-zinc-600 dark:text-zinc-400">
           <em>Rides</em>
-          &nbsp;is my take on building and visualizing a distributed system.
-          Read more at
+          &nbsp;is my take on building and visualizing a scalable system. Read
+          more at
           <a
             className="text-blue-500 hover:text-blue-600 transition-colors"
             href="https://jurajmajerik.com"

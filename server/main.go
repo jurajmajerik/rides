91fda0eda3cf9f03b492752327e2c5e8fc717f61
Juraj Majerik <juro.majerik@gmail.com>
2023-03-28 17:55:52 +0200

    tweak proxy

--- server/main.go
+++ server/main.go
@@ -123,7 +123,7 @@ func main() {
 	db.InitDB()
 	defer db.Connection.Close()
 
-	grafanaURL, _ := url.Parse("http://host.docker.internal:3000")
+	grafanaURL, _ := url.Parse("https://host.docker.internal:3000")
 	grafanaProxy := httputil.NewSingleHostReverseProxy(grafanaURL)
 
 	http.Handle("/", http.FileServer(http.Dir("../frontend/build")))

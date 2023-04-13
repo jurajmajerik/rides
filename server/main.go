bb4179f45a777baa6ea8214d8e010d23fbfa97bd
Juraj Majerik <juro.majerik@gmail.com>
2023-04-13 18:55:29 +0200

    build

--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"strings"
 	"os"
+	"path/filepath"
 	"fmt"
 
 	"github.com/joho/godotenv"
@@ -145,6 +146,49 @@ func getPrometheusProxy() *httputil.ReverseProxy {
 	return prometheusProxy
 }
 
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    // get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+        // if we failed to get the absolute path respond with a 400 bad request
+        // and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+    // prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+    // check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+        // if we got an error (that wasn't that the file doesn't exist) stating the
+        // file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+    // otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func main() {
 	err := godotenv.Load("../.env")
   if err != nil {
@@ -159,16 +203,16 @@ func main() {
 	grafanaProxy := getGrafanaProxy()
 	prometheusProxy := getPrometheusProxy()
 	
-	http.HandleFunc("/api/drivers", getDrivers)
-	http.HandleFunc("/api/customers", getCustomers)
+	router.HandleFunc("/api/drivers", getDrivers)
+	router.HandleFunc("/api/customers", getCustomers)
 	
-	http.HandleFunc("/grafana/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/grafana/", func(w http.ResponseWriter, r *http.Request) {
 		// Modify the incoming request URL to remove the "/grafana" prefix.
     r.URL.Path = strings.TrimPrefix(r.URL.Path, "/grafana")
     grafanaProxy.ServeHTTP(w, r)
 	})
 	
-	http.HandleFunc("/prometheus/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/prometheus/", func(w http.ResponseWriter, r *http.Request) {
 		// Modify the incoming request URL to remove the "/grafana" prefix.
     // r.URL.Path = strings.TrimPrefix(r.URL.Path, "/prometheus")
 		// don't overwrite if asking for /metrics!
@@ -176,7 +220,10 @@ func main() {
 	})
 
 	// http.Handle("/", http.FileServer(http.Dir("../frontend/build")))
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/build")))
+	// router.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/build")))
+
+	spa := spaHandler{staticPath: "../frontend/build", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
 
 	serverEnv := os.Getenv("SERVER_ENV")
 	if serverEnv == "DEV" {

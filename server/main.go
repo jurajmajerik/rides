9edfc3ecf240de9cf861f2bca1878e5631b8be1f
Juraj Majerik <juro.majerik@gmail.com>
2023-04-13 20:36:32 +0200

    build

--- server/main.go
+++ server/main.go
@@ -178,10 +178,12 @@ func main() {
 	router.HandleFunc("/api/drivers", getDrivers)
 	router.HandleFunc("/api/customers", getCustomers)
 
-	router.HandleFunc("/grafana/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/grafana", func(w http.ResponseWriter, r *http.Request) {
 		// Modify the incoming request URL to remove the "/grafana" prefix.
 		// r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		fmt.Println(r.URL.Path)
     r.URL.Path = strings.TrimPrefix(r.URL.Path, "/grafana")
+		fmt.Println(r.URL.Path)
     grafanaProxy.ServeHTTP(w, r)
 	})
 

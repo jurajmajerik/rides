21a29b147548ab1c9ef6660cb3697f9f1e08eccd
Juraj Majerik <juro.majerik@gmail.com>
2023-04-13 21:36:08 +0200

    build

--- server/main.go
+++ server/main.go
@@ -194,6 +194,7 @@ func main() {
 		r.URL.Host = url.Host
 		r.URL.Scheme = url.Scheme
 		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		fmt.Println(r.URL.Host, r.Header.Get("Host"))
 		r.Header.Set("Host", r.URL.Host)
 		r.Host = url.Host
 
